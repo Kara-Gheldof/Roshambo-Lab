2d038fb39541c42683d2362b21a71ad297f63313
Kara G <Kara G@LAPTOP-434UE0L2>
2022-11-10 18:21:07 -0500

    Add project files.

---
+++ Roshambo Lab/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roshambo_Lab
+{
+
+    enum Roshambo
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+    public abstract class Player
+    {
+        public abstract string Name { get; set; }
+        public abstract string RoshamboValue { get; set; }
+
+        
+        string GenerateRoshambo (string roshamboValue)
+        { 
+            RoshamboValue = roshamboValue;
+            return GenerateRoshambo (RoshamboValue);
+        }
+
+        public class RockPlayer : Player
+        {
+            public string RoshamboValue { get; set; }
+             
+        }
+
+        public class RandomPlayer : Player
+        {
+            public string RoshamboValue { get; set; }
+        }
+
+        public class HumanPlayer : Player
+        {
+            public string RoshamboValue { get; set; }
+        }
+    }
+}
